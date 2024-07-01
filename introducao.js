5bdcae4cb521c71cad90ff4eeec86f8454388c7b
Guillan1000 <souguiga@gmail.com>
2024-07-01 10:49:21 -0300

    Teste da chamada de função com o pacote Prompt sync

---
+++ introducao.js
@@ -0,0 +1,13 @@
+function soma(num, num2){
+    let resultado= num + num2
+    console.log('Resultado: '+resultado);
+}
+const prompt = require('prompt-sync')()
+let num = +prompt('Digite um numero')
+
+let nume = +prompt('Digite outro numero')
+
+soma(num, nume)
+   
+    
+
